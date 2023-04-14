bcb5a4e616e19d9005c64034a589da8ac034d3e5
Scott Falbo <scottfalboart@gmail.com>
2023-04-13 22:52:44 -0700

    updated page titles

---
+++ NovumArcanum/Pages/Admin/Ingressus.cshtml.cs
@@ -0,0 +1,15 @@
+///---------------------------------------
+/// Novum Arcanum: Studio Arcanum Seattle
+/// --------------------------------------
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NovumArcanum.Pages.Admin
+{
+    public class IngressusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NovumArcanum/Pages/Admin/Inscribere.cshtml.cs
@@ -0,0 +1,15 @@
+///---------------------------------------
+/// Novum Arcanum: Studio Arcanum Seattle
+/// --------------------------------------
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NovumArcanum.Pages.Admin
+{
+    public class InscribereModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
