36b9d91b5f18acbf8ccc388d1c5e7182d16ce740
Scott Falbo <scottfalboart@gmail.com>
2023-04-13 22:26:33 -0700

    added basic pages

---
+++ NovumArcanum/Pages/Admin/SecretLair.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NovumArcanum.Pages.Admin
+{
+    public class SecretLairModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NovumArcanum/Pages/PraxisVitae.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NovumArcanum.Pages
+{
+    public class PraxisVitaeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
