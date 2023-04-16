bb598d6d0d37df10597fff69a3f13e34b590ed14
Scott Falbo <scottfalboart@gmail.com>
2023-04-16 01:03:58 -0700

    added and updated some models

---
+++ Mechanisms/Models/Constants/Cosmos.cs
@@ -0,0 +1,21 @@
+﻿///---------------------------------------
+/// Novum Arcanum: Studio Arcanum Seattle
+/// --------------------------------------
+
+namespace Mechanisms.Models.Constants
+{
+    public class Cosmos
+    {
+        public class Database
+        {
+            public const string Arcanum = nameof(Arcanum);
+        }
+
+        public class Container
+        {
+            public const string Wizard = nameof(Wizard);
+
+            public const string Sanctum = nameof(Sanctum);
+        }
+    }
+}
--- Mechanisms/Repository/InscriptioRepository.cs
+++ Mechanisms/Repository/InscriptioRepository.cs
@@ -2,9 +2,22 @@
 /// Novum Arcanum: Studio Arcanum Seattle
 /// --------------------------------------
 
+using Mechanisms.Models.Constants;
+using MechanistTower.Clients;
+using Microsoft.Azure.Cosmos;
+
 namespace Mechanisms.Repository
 {
     public class InscriptioRepository : IInscriptioRepository
     {
+        private readonly Container _container;
+
+        public InscriptioRepository(ICosmosTomeScryer cosmosTomeScryer)
+        {
+            var client = cosmosTomeScryer.Scryer();
+            _container = client.GetContainer(Cosmos.Database.Arcanum, Cosmos.Container.Wizard);
+        }
+
+
     }
 }
